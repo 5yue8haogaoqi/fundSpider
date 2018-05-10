9c67598ed86ed2651854b6f8466be558bbfd339b
5yue8haogaoqi <276786979@qq.com>
2018-05-10 22:48:56 +0800

    [+]some codes about stock

--- fundSpider/haomai.py
+++ fundSpider/haomai.py
@@ -1,18 +1,16 @@
 from urllib import request,parse
 
 
-test_html_save_path = 'D:\\note\\markdown\\test.html'
+test_html_save_path = '/Users/Fiona/github/blogs/'
 
 #爬虫对应的网址
 spider_url_path = 'https://www.howbuy.com/fund/ajax/fundranking/index.htm'
-#待post传入的参数，需要替换orderField后面的占位符的参数
-params = [('glrm',''),('keyword',''),('radio',''),('bd',''),('ed',''),('orderType','desc')]
 
 
 
 def getRequestResult(params, url=spider_url_path):
     post_params = parse.urlencode(params)
-    post_params = post_params.encode(encoding='utf-8')
+    post_params = post_params.encode()
     with request.urlopen(url, data=post_params) as f:
         b = f.read()
         content = b.decode('utf-8')
@@ -20,19 +18,46 @@ def getRequestResult(params, url=spider_url_path):
 
 def select_gupiao():
     #构建post参数
-    p1 = ('orderField','hb1z')
-    p2 = ('cat','gupiao.htm')
-    p = list()
-    p.extend(params)
-    p.extend(p1)
-    p.extend(p2)
+    params = Parameters()
+    for p,ind in params:
+        re = getRequestResult(p)
+        with open(test_html_save_path+ind+'.html', mode='w') as f:
+            f.write(re)
 
-    re = getRequestResult(p)
-    with open(test_html_save_path, mode='w', encoding='urf-8') as f:
-        f.write(re)
 
 def select_zhiaquan():
     pass
+
+def search_topN(topN=20):
+    """
+    根据正则匹配topN的基金的代码
+    :param topN: 默认返回top20的基金代码LIST
+    :return: LIST, for example ['1120','1103'...]
+    """
+
+
+
+class Parameters(object):
+
+    def __init__(self):
+        self._variation = ['hb1z','hb1y','hb3y','hb6y','hb1n','hbjn']
+
+    def __iter__(self):
+        return  self
+
+    def __next__(self):
+        try:
+            params = [('glrm', ''), ('keyword', ''), ('radio', ''), ('bd', ''), ('ed', ''), ('orderType', 'desc')]
+            p0 = self._variation.pop()
+            p1 = ['orderField']
+            p1.append(p0)
+            p1 = tuple(p1)
+            p2 = ('cat', 'gupiao.htm')
+            params.append(p1)
+            params.append(p2)
+            return params, p0
+        except:
+            raise StopIteration
 
 
 if __name__ == '__main__':
