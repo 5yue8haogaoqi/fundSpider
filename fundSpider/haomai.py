ff60da415b96c5d7c7aed4c3b0ef6ce0663d9390
frank <congxu_frank@163.com>
2018-05-11 14:32:18 +0800

    [*]股票型基金选基金方法

--- fundSpider/haomai.py
+++ fundSpider/haomai.py
@@ -1,11 +1,11 @@
 from urllib import request,parse
+import re
 
-
-test_html_save_path = '/Users/Fiona/github/blogs/'
 
 #爬虫对应的网址
 spider_url_path = 'https://www.howbuy.com/fund/ajax/fundranking/index.htm'
 
+import datetime
 
 
 def getRequestResult(params, url=spider_url_path):
@@ -16,38 +16,72 @@ def getRequestResult(params, url=spider_url_path):
         content = b.decode('utf-8')
         return content
 
-def select_gupiao():
+def select_gupiao(topN=20):
     #构建post参数
     params = Parameters()
+    fundL = list()
     for p,ind in params:
-        re = getRequestResult(p)
-        with open(test_html_save_path+ind+'.html', mode='w') as f:
-            f.write(re)
+        result= getRequestResult(p)
+        # with open(test_html_save_path+ind+'.html', mode='w') as f:
+        #     f.write(result)
+        codeL = search_topN(result,topN)
+        fundL.append(codeL)
+    return fundL,get_intersection(fundL)
 
+def get_intersection(vList):
+    """
+    查看list中的每个list的交集
+    :param vList:
+    :return: 共同元素组成的list
+    """
+    intersaction = set()
+    for i,val in enumerate(vList):
+        if i == 0:
+            intersaction = set(val)
+        else:
+            intersaction = intersaction & set(val)
+    return intersaction
 
 def select_zhiaquan():
     pass
 
-def search_topN(topN=20):
+def search_topN(html_content,topN=20):
     """
     根据正则匹配topN的基金的代码
     :param topN: 默认返回top20的基金代码LIST
     :return: LIST, for example ['1120','1103'...]
     """
+    html_content = html_content.replace('\t','')
+    html_content = html_content.replace('\r','')
+    html_content = html_content.replace('\n','')
+    html_content = html_content.replace(' ','')
+    regex_str = r'<table.*><tbody>(.*)</tbody></table>'
+    pattern = re.compile(regex_str)
+    result = re.match(pattern, html_content)
 
-
+    regex_code = r'>(\d{6})<'
+    pattern = re.compile(regex_code)
+    code_iter = pattern.finditer(result.group(1))
+    codeL = list()
+    for code in code_iter:
+        codeL.append(code.group(1))
+    codeNum = len(codeL)
+    if codeNum < topN :
+        print ('你设定的数量超过返回结果，请重新确认')
+        return []
+    return codeL[0:topN]
 
 class Parameters(object):
 
     def __init__(self):
-        self._variation = ['hb1z','hb1y','hb3y','hb6y','hb1n','hbjn']
+        self._variation = ['hb1z','hb1y','hb3y','hb6y','hb1n','hbjn','hbRange']
 
     def __iter__(self):
         return  self
 
     def __next__(self):
         try:
-            params = [('glrm', ''), ('keyword', ''), ('radio', ''), ('bd', ''), ('ed', ''), ('orderType', 'desc')]
+            params = [('glrm', ''), ('keyword', ''), ('radio', ''), ('orderType', 'desc')]
             p0 = self._variation.pop()
             p1 = ['orderField']
             p1.append(p0)
@@ -55,10 +89,38 @@ class Parameters(object):
             p2 = ('cat', 'gupiao.htm')
             params.append(p1)
             params.append(p2)
+            """
+            hbRange说明是动态时间的参数，bd和ed这两个参数有变化
+            """
+            if p0 == 'hbRange':
+                params.extend(self.get_date_parameter(True))
+            else:
+                params.extend(self.get_date_parameter())
             return params, p0
         except:
             raise StopIteration
 
+    def _current_date(self):
+        now = datetime.datetime.now()
+        return now.strftime('%Y-%m-%d')
+
+    def _three_year_ago(self):
+        now = datetime.datetime.now()
+        year = now.year
+        month =now.month
+        day = now.day
+        result_format ='{}-{}-{}'
+        return result_format.format((int(year)-3),month,day)
+
+    def get_date_parameter(self,ifNeedValue=False):
+        """
+        获取时间类型的参数
+        :return:
+        """
+        if ifNeedValue:
+            return [('bd',self._three_year_ago()),('ed',self._current_date())]
+        return [('bd', ''), ('ed', '')]
 
 if __name__ == '__main__':
-    select_gupiao()
+    fundL,intersection = select_gupiao(40)
+    print (intersection)
